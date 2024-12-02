e06c4728e21aed27e540c1603c35781b5b49e05e
Simon Oliver <simon@handcircus.com>
2024-12-02 18:25:29 +0000

    Small tidy up

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,8 @@ use std::num::ParseIntError;
 use std::collections::HashMap;
 
 // From https://doc.rust-lang.org/rust-by-example/error/result/enter_question_mark.html
+
+// Split the string line into a pair of integers. Throw a ParseIntError error if not parsed
 fn split_int_pair(in_string: &str) -> Result<(i32,i32), ParseIntError> {
     let mut splitter = in_string.splitn(2, "   ");
     let first = splitter.next().unwrap().parse::<i32>()?;
@@ -11,6 +13,7 @@ fn split_int_pair(in_string: &str) -> Result<(i32,i32), ParseIntError> {
     Ok((first, second))
 }
 
+// Build a map of integers
 fn build_integer_map(int_vector : &Vec<i32>) -> HashMap<i32,i32> {
     let mut value_count_map = HashMap::<i32,i32>::new();
 
@@ -29,7 +32,9 @@ fn build_integer_map(int_vector : &Vec<i32>) -> HashMap<i32,i32> {
 
 fn main() -> std::io::Result<()>{
     let path = env::current_dir()?;
-    println!("Hello, world! from dir {}", path.display());
+    println!("Loading data from dir {}", path.display());
+
+    // Parse data file
 
     let contents = fs::read_to_string("data/input")
         .expect("Should have been able to read the file");
@@ -45,18 +50,20 @@ fn main() -> std::io::Result<()>{
         match split_int_pair(line) {
             Ok((left_column_value,right_column_value))  =>
                 {
-                    //println!("values are is {left_column_value},{right_column_value}");
                     left_column_values.push(left_column_value);
                     right_column_values.push(right_column_value);
                 },
             Err(e) => println!("Error: {}", e),
         }
     }
+    // Ensure line count is equal for both columns
     assert_eq!(left_column_values.iter().count(), right_column_values.iter().count());
 
+    // Sort values
     left_column_values.sort();
     right_column_values.sort();
 
+    // Calculate total distance
     let left_iter = left_column_values.iter();
     let mut right_iter = right_column_values.iter();
     let mut total_distance=0;
@@ -69,24 +76,18 @@ fn main() -> std::io::Result<()>{
 
     println!("Total distance {total_distance}");
 
-    // Calculate similarity
-    let left_count_map=build_integer_map(&left_column_values);
-    let right_count_map=build_integer_map(&right_column_values);
+    // Part 2 - Calculate similarity
 
+    let right_count_map=build_integer_map(&right_column_values);
     let mut total_similarity=0;
 
     for left_val in left_column_values.iter() {
         if (right_count_map.contains_key(left_val))
         {
             let right_count=right_count_map.get(left_val).unwrap();
-            // Multiply
+            // Multiply count by left value and add
             total_similarity+=left_val*right_count;
-            println!("Number {left_val} found count {right_count}");
         }
-        else {
-            println!("Number {left_val} not found in map")
-        }
-
     }
 
     println!("Total similarity {total_similarity}");
